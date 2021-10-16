a27319914580106b101568ba95aa1c0326dceb1f
Jared Kraus <jared.kraus3@gmail.com>
2021-10-15 19:55:15 -0600

    added jk work

--- JK/lab4ExB.cpp
+++ JK/lab4ExB.cpp
@@ -1,18 +1,18 @@
 //  ENSF 614  Lab4 - exercise B
 // lab4ExB.cpp
 
-#include<vector>
-#include<string>
+#include <vector>
+#include <string>
 #include <iostream>
-using std::cout;
 using std::cerr;
+using std::cout;
 using std::endl;
-using std::vector;
 using std::string;
+using std::vector;
 
 typedef vector<string> String_Vector;
 
-String_Vector transpose(const String_Vector& sv);
+String_Vector transpose(const String_Vector &sv);
 // REQUIRES:
 //    sv.size() >= 1
 //    All the strings in sv are the same length, and that length is >= 1.
@@ -20,45 +20,59 @@ String_Vector transpose(const String_Vector& sv);
 //    Return value is the "transpose" of sv, as defined in the Exercise B
 //    instructions.
 
-int main() {
-    
+int main()
+{
+
     const int ROWS = 5;
     const int COLS = 4;
-    
+
     char c = 'A';
     String_Vector sv;
     sv.resize(ROWS);
-    
-    for(int i = 0; i < ROWS; i++)
-        for(int j = 0; j < COLS; j++) {
+
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++)
+        {
             sv.at(i).push_back(c);
             c++;
-            if(c == 'Z' + 1)
+            if (c == 'Z' + 1)
                 c = 'a';
             else if (c == 'z' + 1)
                 c = 'A';
         }
-    
-    
-    for(int i = 0; i < ROWS; i++) {
-        cout<< sv.at(i);
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        cout << sv.at(i);
         cout << endl;
     }
-    
+
     String_Vector vs = transpose(sv);
-    for(int i = 0; i < (int)vs.size(); i++)
+    for (int i = 0; i < (int)vs.size(); i++)
         cout << vs.at(i) << endl;
-    
+
     return 0;
 }
 
+String_Vector transpose(const String_Vector &sv)
+{
 
-
-String_Vector transpose (const String_Vector& sv) {
-    
     // STUDENTS MUST COMPLETE THE DEFINITION OF THIS FUNCTION.
-     
+    int ROWS = (int)sv.size();
+    int COLS = (int)sv.at(0).size();
+
+    int new_ROWS = COLS;
+    int new_COLS = ROWS;
+
     String_Vector vs;
+    vs.resize(new_ROWS);
+    for (int i = 0; i < new_ROWS; i++)
+    {
+        for (int j = 0; j < new_COLS; j++)
+        {
+            vs.at(i).push_back(sv.at(ROWS - 1 - j).at(COLS - 1 - i));
+        }
+    }
+
     return vs;
-    
 }
--- JK/lab4ExC.cpp
+++ JK/lab4ExC.cpp
@@ -6,17 +6,18 @@
 
 const int size = 6;
 using namespace std;
-struct City {
+struct City
+{
     double x, y;
     char name[30];
 };
 
-void write_binary_file(City cities[], int size, char* filename);
+void write_binary_file(City cities[], int size, char *filename);
 /* PROMISES: attaches an ofstream object to a binary file named "filename" and
  * writes the content of the array cities into the file.
  */
 
-void print_from_binary(char* filename);
+void print_from_binary(char *filename);
 /* PROMISES: uses ifstream library object to open the binary file named
  * "filename", reads the content of the file which are objects of struct City
  * (one record at a time), and displays them on the screen. It also saves the records
@@ -24,40 +25,68 @@ void print_from_binary(char* filename);
  * of .txt
  */
 
-
-int main() {
+int main()
+{
     char bin_filename[] = "cities.bin";
-    
+
     City cities[size] = {{100, 50, "Calgary"},
-        {100, 150, "Edmonton"},
-        {50, 50, "Vancouver"},
-        {200, 50, "Regina"},
-        {500, 50, "Toronto"},
-        {200, 50, "Montreal"}};
-    
+                         {100, 150, "Edmonton"},
+                         {50, 50, "Vancouver"},
+                         {200, 50, "Regina"},
+                         {500, 50, "Toronto"},
+                         {200, 50, "Montreal"}};
+
     write_binary_file(cities, size, bin_filename);
     cout << "\nThe content of the binary file is:" << endl;
     print_from_binary(bin_filename);
     return 0;
 }
 
-void write_binary_file(City cities[], int size, char* filename){
+void write_binary_file(City cities[], int size, char *filename)
+{
     ofstream stream(filename, ios::out | ios::binary);
-    if(stream.fail()){
+    if (stream.fail())
+    {
         cerr << "failed to open file: " << filename << endl;
         exit(1);
     }
-    
-    for(int i =0; i < size; i++)
-        stream.write((char*)&cities[i], sizeof(City));
+
+    for (int i = 0; i < size; i++)
+        stream.write((char *)&cities[i], sizeof(City));
     stream.close();
 }
 
-void print_from_binary(char* filename) {
+void print_from_binary(char *filename)
+{
     /* Studnets must complete the implementaiton of this file. */
+    ifstream in_stream(filename, ios::in | ios::binary);
+    if (in_stream.fail())
+    {
+        cout << "The In Stream Failed\n";
+    }
 
+    ofstream out_stream("output.txt");
+    if (out_stream.fail())
+    {
+        cout << "The Out Stream Failed\n";
+    }
+
+    City temp;
+
+    while (!in_stream.eof())
+    {
+        in_stream.read((char *)(&temp), sizeof(City));
+
+        if (in_stream)
+        {
+            cout << "Name: " << temp.name << ", x coordinate: "
+                 << temp.x << ", y coordinate: " << temp.y << endl;
+
+            out_stream << "Name: " << temp.name << ", x coordinate: "
+                       << temp.x << ", y coordinate: " << temp.y << endl;
+        }
+    }
+
+    in_stream.close();
+    out_stream.close();
 }
-
-
-
-
